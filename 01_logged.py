c870f1e28fdef09dcb8c8370c8c4806ead63bdb6
Stiliyan St <65528270+stiliyan172@users.noreply.github.com>
2022-03-24 20:44:09 +0200

    Add files via upload

---
+++ 01_logged.py
@@ -0,0 +1,24 @@
+def logged(func_ref):
+    def wrapper(*args):
+        func_name = func_ref.__name__
+        func_result = func_ref(*args)
+
+        return f"you called {func_name}{args}\nit returned {func_result}"
+
+    return wrapper
+
+
+@logged
+def func(*args):
+    return 3 + len(args)
+
+
+print(func(4, 4, 4))
+
+
+@logged
+def sum_func(a, b):
+    return a + b
+
+
+print(sum_func(1, 4))
