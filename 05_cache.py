cdd482de96faa228249250ab07b9a4836a997e94
Stiliyan St <65528270+stiliyan172@users.noreply.github.com>
2022-03-24 20:44:54 +0200

    Add files via upload

---
+++ 05_cache.py
@@ -0,0 +1,28 @@
+def cache(func_ref):
+    log = {}
+
+    def wrapper(number):
+        if number in log:
+            return log[number]
+        result = func_ref(number)
+        log[number] = result
+
+        return result
+
+    wrapper.log = log
+    return wrapper
+
+
+@cache
+def fibonacci(n):
+    if n < 2:
+        return n
+    else:
+        return fibonacci(n - 1) + fibonacci(n - 2)
+
+
+fibonacci(101)
+print(fibonacci.log)
+
+fibonacci(100)
+print(fibonacci.log)
